0d88b92dace4e92b6d26748082dbd720a7a6463f
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2019-02-22 14:21:12 -0500

    solved question 2

--- prolog.pl
+++ prolog.pl
@@ -1,3 +1,5 @@
+%PROBLEM 1
+
 %if the list is empty, return 0.
 sum-up-numbers-simple([],0).
 
@@ -14,3 +16,35 @@ sum-up-numbers-simple([H|T], N):-
 	sum-up-numbers-simple(T, SUM),
 	%add the first element.
     N is H + SUM.
+
+
+
+
+
+%PROBLEM 2
+
+%if the list is empty, return 0.
+sum-up-numbers-general([],0).
+
+sum-up-numbers-general([H|T], N):- 
+	%if not number or a list, recursively call the function and add zero.
+	not(number(H)),
+	sum-up-numbers-general(T, SUM), 
+	%add zero.
+	N is 0 + SUM.
+
+sum-up-numbers-general([H|T], N):-
+	%if the element is list, call the function with the first element
+	is_list(H),
+	sum-up-numbers-general(H, SUM1),
+	%call the function with the first element removed
+	sum-up-numbers-general(T, SUM),
+	%add the sum of the first element and the remamining element
+	N is 0 + SUM1 + SUM.
+
+sum-up-numbers-general([H|T], N):- 
+	%if number, call the function recursively with rest of the elements in the function and add the first element to sum.
+	number(H),
+	sum-up-numbers-general(T, SUM),
+	%we add the first element
+	N is H + SUM.
